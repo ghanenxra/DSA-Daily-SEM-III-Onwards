5c0073036120eb8afb21c063d9165fee4fc3c388
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-08-10 05:22:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (52.37%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -5,7 +5,7 @@ class Solution:
         
         prev2, prev1 = 1, 2
         
-        for _ in range(3, n + 1):
+        for i in range(3, n + 1):
             curr = prev1 + prev2
             prev2 = prev1
             prev1 = curr
