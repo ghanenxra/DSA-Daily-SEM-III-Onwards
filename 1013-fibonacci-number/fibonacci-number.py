2a345ac00bc43edfdec4cf3ef27a4e1e27667505
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-08-10 19:55:05 +0530

    Time: 351 ms (5.13%) | Memory: 19.1 MB (57.12%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -5,4 +5,7 @@ class Solution:
         if n == 1:
             return 1
 
-        return self.fib(n-1) + self.fib(n-2)
+        first_ele = self.fib(n-1)
+        second_ele = self.fib(n-2)
+
+        return first_ele + second_ele
