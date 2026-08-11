1f18afe4242e0e9ea6f519923c519c18be167ad6
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-08-12 00:21:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (72.49%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -26,23 +26,3 @@ class Solution:
     
 
 
-
-        # new_node = ListNode()
-        # curr = new_node
-
-        # while list1 and list2:
-        #     if list1.val > list2.val:
-        #         curr.next = list2
-        #         list2 = list2.next
-
-        #     else:
-        #         curr.next = list1
-        #         list1 = list1.next
-        #     curr = curr.next
-
-        # if list1:
-        #     curr.next = list1
-        # else:
-        #     curr.next = list2 
-
-        # return new_node.next
