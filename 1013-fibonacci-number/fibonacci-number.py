778397aeb002996708c2f21f9e4da2399f801a1a
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-09-06 14:16:26 +0530

    Time: 316 ms (25.93%) | Memory: 19 MB (98.16%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,14 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-
-        first_ele = self.fib(n-1)
-        second_ele = self.fib(n-2)
-
-        return first_ele + second_ele
-
-        # return n if n<=1 else self.fib(n-1)+self.fib(n-2) 
-        """ Also did this one in single line """
+        if n==1 or n==0:
+            return n
+        
+        return self.fib(n-1)+self.fib(n-2)
