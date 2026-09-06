ec738cd17c32c97b9e49022540dc979af4042a5c
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-09-06 16:39:22 +0530

    Time: 46 ms (67.01%) | Memory: 19.1 MB (57.97%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,5 +1,10 @@
+cache={}
 class Solution:
     def fib(self, n: int) -> int:
+        global cache
         if n==1 or n==0:
             return n
-        return self.fib(n-1)+self.fib(n-2)
+        if n in cache:
+            return cache[n]
+        cache[n]=self.fib(n-1)+self.fib(n-2)
+        return cache[n]
