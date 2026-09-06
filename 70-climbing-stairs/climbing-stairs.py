cae890e10e1cf518b4c5e656a1b510dcdb59ba33
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-09-06 22:28:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (53.50%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,19 +1,12 @@
+cache = {}
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # if n==0 or n==1:
-        #     return 1
-
-        # return self.climbStairs(n-1) + self.climbStairs(n-2)
-
-        if n <= 2:
-            return n
-        
-        prev_step1 = 2
-        prev_step2 = 1
-        
-        for i in range(3, n + 1):
-            total_ways = prev_step1 + prev_step2
-            prev_step2 = prev_step1
-            prev_step1 = total_ways
-        
-        return prev_step1
+        global cache
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        if n in cache:
+            return cache[n]
+        cache[n] = self.climbStairs(n-1)+self.climbStairs(n-2)
+        return cache[n]
