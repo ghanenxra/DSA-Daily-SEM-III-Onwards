3b2740ef84c284e01836e11778bae0ba8f452e64
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-09-14 17:22:50 +0530

    Time: 8 ms (6.71%) | Memory: 21.7 MB (13.03%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -11,3 +11,12 @@ class Solution:
             cache[i]=cost[i]+min(less_cost(i-1), less_cost(i-2))
             return cache[i]
         return min(less_cost(n-1), less_cost(n-2))
+
+ 
+        
+        # def min_cost(i):
+        #     n=len(cost)
+        #     if i<=1:
+        #         return cost[i]
+        #     return cost[i]+min(min_cost(i-1), min_cost(i-2))
+        # return min(min_cost(n-1), min_cost(n-2))
