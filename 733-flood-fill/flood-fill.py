f970455b7f411d55d6d8c8730c2b46803323cc9a
Ghanendra Chakradhari <pureghanendra@gmail.com>
2026-09-26 00:49:37 +0530

    Time: 3 ms (20.08%) | Memory: 19.2 MB (97.44%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class Solution:
+    def floodFill(self, image: list[list[int]], sr: int, sc: int, color: int) -> list[list[int]]:
+        og_color = image[sr][sc]
+        if og_color == color:
+            return image
+        
+        m, n = len(image), len(image[0])
+        queue = deque([(sr, sc)])
+        image[sr][sc] = color
+        
+        while queue:
+            r, c = queue.popleft()
+            for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n and image[nr][nc] == og_color:
+                    image[nr][nc] = color
+                    queue.append((nr, nc))
+                    
+        return image
